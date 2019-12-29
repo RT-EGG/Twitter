adff5af6f71ff0c7156a0f673b18b69064c56da5
RT-EGG <rt.egg.jp@gmail.com>
2019-12-30 00:06:32 +0900

    refactoring

--- Imetter/Imetter/CtrlUserInfoView.cs
+++ Imetter/Imetter/CtrlUserInfoView.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CoreTweet;
 using System.Windows.Forms;
-using CoreTweet;
-using System.Net;
 
 namespace TwitterTimeLine
 {
